b9f8edf9ea010b15c58ff60f1c52e6d36230e7a7
| Ali soleymani | <alisoleimani199@gmail.com>
2022-03-15 21:59:20 +0330

    Import tsx and style files

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "~store/store";
+import store from "./core/store/store";
 import "./core/localization";
 
-import App from "./App";
+import App from "./pages/App";
+
+import "./assets/css/app.css"
+import "./assets/scss/app.scss"
 
 ReactDOM.render(
   <React.StrictMode>
