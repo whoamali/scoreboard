b4a6141cf3397c9c26931a48dd83258324507213
| Ali soleymani | <alisoleimani199@gmail.com>
2022-03-15 21:46:31 +0330

    feat: Install tailwindcss and added config file

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/pages/**/*.{html,js}", "./src/components/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Fredoka: ["Fredoka", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
