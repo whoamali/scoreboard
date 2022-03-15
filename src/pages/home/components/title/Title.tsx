f5247e1defcc237bfc4f185e68b5d82d75d082dd
| Ali soleymani | <alisoleimani199@gmail.com>
2022-03-16 01:30:06 +0330

    Create homepage title components

---
+++ src/pages/home/components/title/Title.tsx
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <p className="text-black font-[600] font-Fredoka text-6xl">
+      Online Free <span className="font-[300] text-slate-900">Score</span>
+      <span className="text-orange-600">Board</span>
+    </p>
+  );
+}
