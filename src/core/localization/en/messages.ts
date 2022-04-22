7202810ef30549a6705fdd5c1ca8f0f41d14390e
| Ali soleymani | <alisoleimani199@gmail.com>
2022-04-22 15:26:38 +0430

    Localizarion

--- src/core/localization/en/messages.ts
+++ src/core/localization/en/messages.ts
@@ -14,15 +14,26 @@ export default {
     "app.createboard.title-label": "title",
     "app.createboard.title-placeholder": "scoreboard",
     "app.createboard.title-required-error": "this field is required",
-    "app.createboard.discription-label": "discription (optional)",
-    "app.createboard.discription-discription": "your scoreboard discription",
-    "app.createboard.discription-placeholder": "discription",
-    "app.createboard.number-of-players-label": "number of players",
-    "app.createboard.number-of-players-discription":
+    "app.createboard.description-label": "description (optional)",
+    "app.createboard.description-description": "your scoreboard description",
+    "app.createboard.description-placeholder": "description",
+    "app.createboard.number-of-players_number-label": "number of players",
+    "app.createboard.number-of-players_number-description":
       "number of participants (cannot exceed 1000 players)",
-    "app.createboard.number-of-players-placeholder": "number",
+    "app.createboard.number-of-players_number-placeholder": "number",
     "app.createboard.unit-label": "score unit",
     "app.createboard.unit-placeholder": "e.g. $ , 🏅 , 🧉",
     "app.createboard.button-label": "create board",
+
+    "app.board.user-key-placeholder": "Enter your user key or board id",
+    "app.board.user-key-required-error": "this field is required",
+    "app.board.user-key-button-label": "go",
+    "app.board.alert-error-message": "connection error",
+
+    "app.board.board-data.alert.create-message": "you can create your own scoreboard! click to create",
+    "app.board.board-player.order": "order",
+    "app.board.board-player.order.most": "most",
+    "app.board.board-player.order.least": "least",
+    "app.board.board-player.table.name": "name",
   },
 };
