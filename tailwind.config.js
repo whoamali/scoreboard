db9f3fd576956de70fd65aeb689533b1fdb07347
| Ali soleymani | <alisoleimani199@gmail.com>
2022-05-15 10:55:42 +0430

    install tw-elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  content: ["./src/pages/**/*.{html,jsx,tsx}", "./src/components/**/*.{html,jsx,tsx}"],
+  content: [
+    "./src/pages/**/*.{html,jsx,tsx}",
+    "./src/components/**/*.{html,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     fontFamily: {
       Fredoka: ["Fredoka", "sans-serif"],
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 };
